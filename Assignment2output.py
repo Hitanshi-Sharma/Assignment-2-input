2d4c798dcf99446df018d4f5c4c33d3105f807c3
Hitanshi-Sharma <103354645+Hitanshi-Sharma@users.noreply.github.com>
2022-05-14 15:26:49 +0530

    Add files via upload

---
+++ Assignment2output.py
@@ -0,0 +1,36 @@
+The length of string is: 35
+The reverse order of the string in one line code is: egaugnal evitisnes esac a si nohtyP
+The new sliced string is: a case sensitive
+The new replaced string is: Python is object oriented language
+The index of substring a is : 10
+The new string is given by: Pythonisacasesensitivelanguage
+
+
+Hey, Hitanshi Sharma Here!
+My SID is 21102025
+I am from Civil department and my CGPA is 9.9
+
+
+Output of a&b is: 8
+Output of a|b is: 58
+Output of a^b is: 50
+Left shift both a and b with 2 bits is: (224, 40)
+Right shift a with 2 bits and b with 4 vits is: (14, 0)
+
+
+Enter the string: Hitanshi 
+No
+
+
+Enter the first side of triangle:3
+Enter the second side of triangle:4
+Enter the third side of triangle:8
+
+
+Enter the number: 14
+Enter the number: 18
+Number of bits needed to be flipped to convert a to b is : 1
+Number of bits needed to be flipped to convert a to b is : 2
+Number of bits needed to be flipped to convert a to b is : 3
+
+Process finished with exit code 0
